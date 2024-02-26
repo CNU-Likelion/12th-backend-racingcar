5eb278219c3831af05cc502d96554cde3493c732
donghyunn1 <dongdong0516@naver.com>
2024-02-26 23:13:54 +0900

    feat: 입력할 때 예외상황 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,23 +2,36 @@ package racingcar;
 
 import mallang.missionutils.Console;
 
+
 public class Application {
-    private static void getUserInput() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        getCarName();
-        System.out.println("시도할 회수는 몇회인가요?");
-        getNumberOfTry();
-    }
-
-    private static void getCarName() {
-        String[] UserArr = Console.readLine().split(",");
-    }
-
-    private static void getNumberOfTry() {
-        int numberOfTry = Integer.parseInt(Console.readLine());
-    }
-
     public static void main(String[] args) {
-        getUserInput();
+        while (true) {
+            try {
+                System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+                String input = Console.readLine();
+                String[] names = input.split(",");
+                for (String name : names) {
+                    if (name.length() > 5) {
+                        System.out.println("[ERROR] 자동차 이름은 5자 이하여야 합니다.");
+                        throw new IllegalArgumentException();
+                    }
+                }
+                break;
+            } catch (IllegalArgumentException e) {
+            }
+        }
+
+        // 시도 횟수 입력 받기
+        int attempts = 0;
+        while (true) {
+            try {
+                System.out.println("시도할 회수는 몇회인가요?");
+                attempts = Integer.parseInt(Console.readLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 시도 횟수는 숫자여야 합니다.");
+            }
+        }
+
     }
 }
