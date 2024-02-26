10b146240b0ccc0fa1d44e3b2fc664d6491979af
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-02-26 14:55:53 +0900

    feat : 사용자 입력 검증 기능 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -15,6 +15,7 @@ public class Application {
     public static List<Car> initializeCars() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNamesInput = Console.readLine();
+        validateCarNames(carNamesInput);
 
         String[] carNames = carNamesInput.split(",");
         List<Car> cars = new ArrayList<>();
@@ -26,9 +27,22 @@ public class Application {
         return cars;
     }
 
+    public static void validateCarNames(String carNames) {
+        if (!carNames.matches("[a-zA-Z,]+") || carNames.contains(",")) {
+            throw new IllegalArgumentException("자동차 이름은 영문자 쉼표(,)로 구분하여 입력하세요.");
+        }
+    }
+
     public static int getTryCount() {
         System.out.println("시도할 횟수는 몇 회인가요?");
         String tryCountInput = Console.readLine();
+        validateTryCount(tryCountInput);
         return Integer.parseInt(tryCountInput);
+    }
+
+    public static void validateTryCount(String tryCount) {
+        if (!tryCount.matches("\\d+")) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 합니다.");
+        }
     }
 }
