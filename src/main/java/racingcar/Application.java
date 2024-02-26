932be3f22e11e67baaead1c8d232674e24cd6b8a
donghyunn1 <dongdong0516@naver.com>
2024-02-26 23:16:01 +0900

    feat: 입력할 때 예외상황 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,6 +5,7 @@ import mallang.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
+        // 자동차 이름 입력받기
         while (true) {
             try {
                 System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
