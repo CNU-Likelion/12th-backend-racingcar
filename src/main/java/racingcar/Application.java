2dd1f84ff3c07955f50c91faaef63cc287efc1aa
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-02-26 14:44:02 +0900

    feat : 사용자에게 자동차 이름과 시도 횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,34 @@
 package racingcar;
 
+import mallang.missionutils.Console;
+import mallang.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO 구현 진행
+    }
+
+    public static List<Car> initializeCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesInput = Console.readLine();
+
+        String[] carNames = carNamesInput.split(",");
+        List<Car> cars = new ArrayList<>();
+
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        return cars;
+    }
+
+    public static int getTryCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String tryCountInput = Console.readLine();
+        return Integer.parseInt(tryCountInput);
     }
 }
