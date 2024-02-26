f9294756782457878818efec7fccace83cafaa19
donghyunn1 <dongdong0516@naver.com>
2024-02-26 22:22:19 +0900

    feat: 자동차의 이름과 시도횟수 입력받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,24 @@
 package racingcar;
 
+import mallang.missionutils.Console;
+
 public class Application {
+    private static void getUserInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        getCarName();
+        System.out.println("시도할 회수는 몇회인가요?");
+        getNumberOfTry();
+    }
+
+    private static void getCarName() {
+        String[] UserArr = Console.readLine().split(",");
+    }
+
+    private static void getNumberOfTry() {
+        int numberOfTry = Integer.parseInt(Console.readLine());
+    }
 
     public static void main(String[] args) {
-        // TODO 구현 진행
+        getUserInput();
     }
 }
