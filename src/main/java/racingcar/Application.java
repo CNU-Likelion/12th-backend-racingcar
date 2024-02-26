411ea061f173173d5d8ed02fb7906fd6b4aedd11
sslynn22 <seoin@iseoin-ui-MacBookPro.local>
2024-02-27 01:48:16 +0900

    feat:클래스 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,57 @@
 package racingcar;
 
+import static racingcar.Win.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import mallang.missionutils.Console;
+import mallang.missionutils.Randoms;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO 구현 진행
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        List<Car> cars = new ArrayList<>();
+        String inputName = Console.readLine();
+        String[] names = inputName.split(",");
+        for (String name : names) {
+            cars.add(new Car(name.trim()));
+        }
+        int gameTurn = getTurn();
+        System.out.println("실행 결과 : ");
+        for (int i=0 ; i < gameTurn ; i++) {
+            for (Car car : cars) {
+                car.carMove();
+                car.printCarStatus(car);
+                System.out.println();
+            }
+            System.out.println();
+        }
+        printWinnerNames(cars);
+
+    }
+    public static void checkTurn(String gameTurn) {
+        if (gameTurn == null || gameTurn.length() == 0) {
+            throw new IllegalArgumentException("[ERROR]숫자를 입력해주세요.");
+        }
+        for (char c : gameTurn.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("[ERROR]숫자를 입력해주세요.");
+            }
+        }
+    }
+
+    public static void checkNegativeNum(int gameTurn) {
+        if (gameTurn <= 0)
+            throw new IllegalArgumentException("[ERROR]0보다 큰 숫자를 입력해주세요.");
+    }
+
+    public static int getTurn() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String inputNum = Console.readLine();
+        checkTurn(inputNum);
+        int gameTurn = Integer.parseInt(inputNum);
+        checkNegativeNum(gameTurn);
+        return gameTurn;
     }
 }
--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,13 +1,52 @@
 package racingcar;
-
+import mallang.missionutils.Randoms;
 public class Car {
 
     private final String name;
     private int position = 0;
 
     public Car(String name) {
+        checkCarName(name);
         this.name = name;
+        this.position = 0;
     }
 
-    // 추가 기능 구현
+    public void checkLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR]5자 이하의 이름을 입력하시오.");
+        }
+    }
+
+    public void checkEmpty(String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR]이름을 입력하시오.");
+        }
+    }
+
+    public void checkCarName(String name) {
+        checkEmpty(name);
+        checkLength(name);
+    }
+
+    public void carMove() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        if (randomNum >= 4) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void printCarStatus(Car car) {
+        System.out.print(car.getName() + " : ");
+        for (int i=0 ; i<car.getPosition() ; i++) {
+            System.out.print("-");
+        }
+    }
 }
