6187f825b03d612fdcf1d096417d3874ccf9d76e
CHAEGODA <chae_goda@naver.com>
2024-02-28 01:08:55 +0900

    feat : Application 1 startGame 함수 생성(자동차 이름과 시도 횟수 입력받고 예외발생시키기)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,32 @@
 package racingcar;
+import java.util.*;
+import mallang.missionutils.Randoms;
+import mallang.missionutils.Console;
 
 public class Application {
-
     public static void main(String[] args) {
         // TODO 구현 진행
+    }
+    public static void startGame() {
+        List <Car> cars = new ArrayList<>();
+
+        String carName = mallang.missionutils.Console.readLine();
+        String[] names = carName.split(",");
+        for(String name : names) {
+            if(name.length() > 5) {
+                System.out.println("[ERROR] 자동차 이름은 5자 이하여야한다.");
+                return;
+            }
+            cars.add(new Car(name));
+        }
+
+        System.out.println("시도 할 횟수");
+        String attemptInput = mallang.missionutils.Console.readLine();
+        int attempt;
+        try {
+            attempt = Integer.parseInt(attemptInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 한다.");
+        }
     }
 }
