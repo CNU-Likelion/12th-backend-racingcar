5775a255603b03346ffbc67a48af21da810a856f
donghyunn1 <dongdong0516@naver.com>
2024-02-27 11:38:22 +0900

    feat: Car에 대한 함수 생성

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import mallang.missionutils.Randoms;
+
 public class Car {
 
     private final String name;
@@ -7,7 +9,29 @@ public class Car {
 
     public Car(String name) {
         this.name = name;
+        this.position = 0;
     }
 
     // 추가 기능 구현
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        int randomNum = Randoms.pickNumberInRange(0, 9);
+        if(randomNum >= 4) {
+            position++;
+        }
+    }
+
+    public void printCarMove(Car car) {
+        System.out.print(car.getName() + " : ");
+        for(int i = 0; i < car.getPosition(); i++) {
+            System.out.print("-");
+        }
+    }
 }
