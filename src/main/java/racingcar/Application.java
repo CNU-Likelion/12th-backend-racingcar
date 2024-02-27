dfdc0413ce05dc4dd21e79971dd1d3fda404d43a
minji <qkralswl6125@naver.com>
2024-02-27 20:12:37 +0900

    feat: 사용자 입력시 예외상황 처리하기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -11,7 +11,19 @@ public class Application {
     private static void getUserInput() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         String[] carNames = Console.readLine().split(",");
+        if (!isValidName(carNames)) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
         System.out.println("시도할 횟수는 몇회인가요?");
         int tryCount = Integer.parseInt(Console.readLine());
+    }
+
+    private static boolean isValidName(String[] carNames) {
+        for (String name : carNames) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+        return true;
     }
 }
