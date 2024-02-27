706e04912ce457757f1973ca43d765ed80c3d8bf
Shsin9797 <aaa8141@naver.com>
2024-02-28 01:17:02 +0900

    fix : 쓰이지 않는 함수 삭제

--- src/main/java/racingcar/GameResult.java
+++ src/main/java/racingcar/GameResult.java
@@ -6,12 +6,6 @@ import java.util.List;
 public class GameResult {
     List<Car> winners = new ArrayList<>();
 
-    public Car findCandidateCar(Car c1, Car c2) {
-        if (c1.getPosition() >= c2.getPosition()) {
-            return c1;
-        }
-        return c2;
-    }
 
     public int findMaxPositionValue(List<Car> carList) {
         int maxPositionValue = carList.get(0).getPosition();
