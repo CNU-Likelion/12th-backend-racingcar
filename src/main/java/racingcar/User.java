f79e71bd5e3598ce0f6c34272ed7c946bc31af7a
Shsin9797 <aaa8141@naver.com>
2024-02-28 01:00:15 +0900

    fix: 차 이름을 받은걸로 차의 객체들을 생성해서 저장하기

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -8,12 +8,19 @@ import static mallang.missionutils.Console.readLine;
 
 public class User {
     List<String> carNames = new ArrayList<>();
+    List<Car> cars = new ArrayList<>();
     int gameCnt;
 
     public void getCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] nameLine = readLine().split(",");
         this.carNames = Arrays.stream(nameLine).toList();
+    }
+
+    public void makeCars() {
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
     }
 
     public void getGameCnt() {
