681f91366257c777285f60c1808a5e55ed7be4b4
Shsin9797 <aaa8141@naver.com>
2024-02-28 00:31:45 +0900

    feat : 두개의 자동차를 비교해서 더 멀리있는 차 객체를 반환하는 메서드 구현

--- src/main/java/racingcar/GameResult.java
+++ src/main/java/racingcar/GameResult.java
@@ -6,5 +6,12 @@ import java.util.List;
 public class GameResult {
     List<Car> winners = new ArrayList<>();
 
+    public Car findCandidateCar(Car c1, Car c2) {
+        if (c1.getPosition() >= c2.getPosition()) {
+            return c1;
+        }
+        return c2;
+    }
+
 
 }
