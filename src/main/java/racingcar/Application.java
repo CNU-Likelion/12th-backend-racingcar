d813ddf038e4a2fa83d141404783661c13821fc5
CHAEGODA <chae_goda@naver.com>
2024-02-28 01:28:06 +0900

    refactor : Application 1 startGame 함수에 입력할 값을 안내해주는 문장추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -9,7 +9,7 @@ public class Application {
     }
     public static void startGame() {
         List <Car> cars = new ArrayList<>();
-
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carName = mallang.missionutils.Console.readLine();
         String[] names = carName.split(",");
         for(String name : names) {
