6ff7e83282385776e09ee973998c80aed62cb9fc
CHAEGODA <chae_goda@naver.com>
2024-02-28 00:10:50 +0900

    feat : Car 클래스에 랜덤한 수(0 ~ 9)를 만들고 그 값이 4이상일 경우 전진하는 moveForward 함수 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,14 +1,21 @@
 package racingcar;
 
+import mallang.missionutils.Randoms;
+
 public class Car {
 
     private final String name;
     private int position = 0;
-    private int move = 0;
 
     public Car(String name) {
         this.name = name;
     }
 
     // 추가 기능 구현
+    public void moveForward() {
+        int move = Randoms.pickNumberInRange(0,9);
+        if(move >= 4) {
+            position += move;
+        }
+    }
 }
