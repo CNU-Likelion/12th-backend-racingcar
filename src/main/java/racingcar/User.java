7e4acffcc5be83ee63f8e2d3b1759884bd8d2aaf
Shsin9797 <aaa8141@naver.com>
2024-02-28 00:16:14 +0900

    fix : 이용자로부터 입력값을 받기위한 안내문구 출력

--- src/main/java/racingcar/User.java
+++ src/main/java/racingcar/User.java
@@ -11,12 +11,14 @@ public class User {
     int gameCnt;
 
     public void getCarName() {
-            String[] nameLine = readLine().split(",");
-            this.carNames = Arrays.stream(nameLine).toList();
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] nameLine = readLine().split(",");
+        this.carNames = Arrays.stream(nameLine).toList();
     }
 
     public void getGameCnt() {
         try {
+            System.out.println("시도할 회수는 몇회인가요?");
             this.gameCnt = Integer.parseInt(readLine());
 
         } catch (IllegalArgumentException e){
