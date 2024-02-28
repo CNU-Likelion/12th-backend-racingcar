34fe839e39982737c26d3209bdfce98c8c50ab29
maxblessed01 <dop9405@naver.com>
2024-02-29 06:33:47 +0900

    first

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,13 +1,64 @@
 package racingcar;
 
+import java.util.Objects;
 public class Car {
 
-    private final String name;
-    private int position = 0;
+    private final Name name;
+    private final Position position;
 
     public Car(String name) {
-        this.name = name;
+        this(name, new Position(0));
     }
 
-    // 추가 기능 구현
+    public Car(String name, Position position) {
+        this.name = new Name(name);
+        this.position = position;
+    }
+
+    public void move() {
+        position.move();
+    }
+
+    public Position getMaxposition(Position position) {
+        if (this.position.lessThan(position)) {
+            return position;
+        }
+        return this.position;
+    }
+
+    public boolean isWinner(Position position) {
+        return this.position.isSame(position);
+    }
+
+    public String getName() {
+        return name.getName();
+    }
+
+    public int getPosition() {
+        return position.getPosition();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Objects.equals(name, car.name) && Objects.equals(position,
+                car.position);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name,position);
+    }
+
+    @Override
+    public String toString() {
+        return "car{"
+                + "name='" + name + '\''
+                + ",position=" + position + '}';
+    }
 }
