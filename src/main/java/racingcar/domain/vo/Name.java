139b83bec155bb6b09b742df065a484d794dcd89
jeomxon <jeomxon@gmail.com>
2024-02-29 16:18:35 +0900

    refactor: 값 객체 equals and hashCode 재정의

--- src/main/java/racingcar/domain/vo/Name.java
+++ src/main/java/racingcar/domain/vo/Name.java
@@ -1,5 +1,7 @@
 package racingcar.domain.vo;
 
+import java.util.Objects;
+
 public record Name(String value) {
 
     private static final int MAXIMUM_LENGTH_OF_NAME = 5;
@@ -12,5 +14,22 @@ public record Name(String value) {
         if (value.isEmpty() || value.length() > MAXIMUM_LENGTH_OF_NAME) {
             throw new IllegalArgumentException("[ERROR] 이름의 길이는 1~5까지만 가능합니다.");
         }
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Name name = (Name) o;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
--- src/main/java/racingcar/domain/vo/Position.java
+++ src/main/java/racingcar/domain/vo/Position.java
@@ -1,8 +1,27 @@
 package racingcar.domain.vo;
 
+import java.util.Objects;
+
 public record Position(int value) {
-    
+
     public Position increase() {
         return new Position(value + 1);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Position position = (Position) o;
+        return value == position.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
