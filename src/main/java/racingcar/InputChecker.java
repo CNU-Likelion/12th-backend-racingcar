c875520c4efec978563883cd474b564f9932d468
alsswl <alswl4223@naver.com>
2024-03-02 00:40:06 +0900

    refactor : 에러메세지 매직리터럴 사용

--- src/main/java/racingcar/InputChecker.java
+++ src/main/java/racingcar/InputChecker.java
@@ -2,10 +2,13 @@ package racingcar;
 
 public class InputChecker {
 
+    final static String NAME_ERROR_MESSAGE = "[ERROR] 이름은 5자 이내로 입력하셔야 합니다.";
+    final static String NUMBER_ERROR_MESSAGE = "[ERROR] 시도 횟수는 숫자여야 합니다.";
+
     public void checkName(String[] userName) {
         for (int i = 0; i < userName.length; i++) {
             if (userName[i].length() > 5) {
-                System.out.println("[ERROR] 이름은 5자 이내로 입력하셔야 합니다.");
+                System.out.println(NAME_ERROR_MESSAGE);
                 throw new IllegalArgumentException();
             }
         }
@@ -15,7 +18,8 @@ public class InputChecker {
         try {
             Integer.parseInt(numberOfTry);
         } catch (NumberFormatException ex) {
-            throw new IllegalArgumentException("[ERROR] 시도 횟수는 숫자여야 합니다.");
+            System.out.println(NUMBER_ERROR_MESSAGE);
+            throw new IllegalArgumentException();
         }
     }
 }
