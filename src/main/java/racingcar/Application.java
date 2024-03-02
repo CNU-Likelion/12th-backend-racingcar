0060cd317bd99cbc5878def6a617fa93bcf84a2f
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-03-03 02:09:29 +0900

    refactor: 불필요 import 제거

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,9 +1,5 @@
 package racingcar;
 
-import mallang.missionutils.Console;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
 
     public static void main(String[] args) {
